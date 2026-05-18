d5cedb8d6f53f6d83fcda9aa40dfec7b792124b2
YeysLarens <yeyslarens@gmail.com>
2026-05-18 12:23:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// alert('Hello World of JavaScript');
+
+// $name ="Juan DelaCruz";
+// document.getElementById("text").innerHTML= $name;
+// console.log($name)
+// alert($name);
+
 let name ="Juan DelaCruz";
 document.getElementById("text").innerHTML= name;
 console.log(name)
