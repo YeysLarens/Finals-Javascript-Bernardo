ef273d3a82daf7b00594af3a763ba6ea1b821d3e
YeysLarens <yeyslarens@gmail.com>
2026-05-18 12:24:00 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,29 +1,11 @@
-function
-getInnerText
-() {
-alert(
-document.getElementById
-("example").
-innerText
-)
+function getInnerText() {
+    alert(document.getElementById("example").innerText);
 }
 
-function
-getInnerHTML
-() {
-alert(
-document.getElementById
-("example").
-innerHTML
-)
+function getInnerHTML() {
+    alert(document.getElementById("example").innerHTML);
 }
 
-function
-getTextContent
-() {
-alert(
-document.getElementById
-("example").
-textContent
-)
+function getTextContent() {
+    alert(document.getElementById("example").textContent);
 }
